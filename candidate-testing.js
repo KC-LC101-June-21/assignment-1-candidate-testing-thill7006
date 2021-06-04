41e7e4411b74a7bcfb6c00cd385b5c5ff0ee8f17
thill4455 <>
2021-06-04 18:00:01 +0000

    added question/input for candidate name and quiz question

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -11,23 +11,31 @@ let candidateAnswer;
 let questions;
 let correctAnswers;
 let candidateAnswers;
-
+question = "Who was the first American woman in space? \n";
+correctAnswer = "Sally Ride";
+candidateAnswer = "";
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+const input = require("readline-sync");
+candidateName = input.question("What is your name? \n \n");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+const input = require("readline-sync");
+candidateAnswer = input.question(question + "\n");
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer === correctAnswer) {
+  console.log("\nYou are correct!")
+} else {
+  console.log("\nYou are wrong.")
+}
 
   let grade;
   
@@ -38,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("\nHello, " + candidateName + "\n");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
